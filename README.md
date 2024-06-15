556020e34831f32771fde8320aa393d98e4efe95
Anthony Pyrtle <xxpyrtlexx93@gmail.com>
2024-06-15 16:27:27 -0400

    add clone script for May

---
+++ README.md
@@ -0,0 +1,5 @@
+Made a script so we don't have to add google-modules 1 by 1. This will also update the tags for the modules to whatever you set it as in the script under branch. 
+To run this script:
+1. give it permission by chmod +x (whatever you named the script)
+2. type ./(name of script) to run it
+3. watch the magic happen
---
+++ clone-tag.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Function to clone repository and move to directory
+clone_and_move() {
+    local repo_url=$1
+    local folder_path=$2
+
+    echo "Cloning $repo_url into folder $folder_path..."
+    git clone --branch android-14.0.0_r0.66 --single-branch "$repo_url" "$folder_path"
+}
+
+# Repositories with custom folder structure
+declare -A custom_repos=(
+    ["https://android.googlesource.com/kernel/google-modules/bluetooth/broadcom"]="bluetooth/broadcom"
+    ["https://android.googlesource.com/kernel/google-modules/bluetooth/qcom"]="bluetooth/qcom"
+    ["https://android.googlesource.com/kernel/google-modules/bluetooth/synaptics"]="bluetooth/synaptics"
+    ["https://android.googlesource.com/kernel/google-modules/edgetpu/abrolhos"]="edgetpu/abrolhos"
+    ["https://android.googlesource.com/kernel/google-modules/edgetpu/janeiro"]="edgetpu/janeiro"
+    ["https://android.googlesource.com/kernel/google-modules/fingerprint/fpc"]="fingerprint/fpc"
+    ["https://android.googlesource.com/kernel/google-modules/gxp/gs201"]="gxp/gs201"
+    ["https://android.googlesource.com/kernel/google-modules/power/reset"]="power/reset"
+    ["https://android.googlesource.com/kernel/google-modules/sensors/hall_sensor"]="sensors/hall_sensor"
+    ["https://android.googlesource.com/kernel/google-modules/touch/common"]="touch/common"
+    ["https://android.googlesource.com/kernel/google-modules/touch/focaltech_touch"]="touch/focaltech_touch"
+    ["https://android.googlesource.com/kernel/google-modules/touch/fts_touch"]="touch/fts_touch"
+    ["https://android.googlesource.com/kernel/google-modules/touch/goodix_touch"]="touch/goodix_touch"
+    ["https://android.googlesource.com/kernel/google-modules/touch/novatek_touch"]="touch/novatek_touch"
+    ["https://android.googlesource.com/kernel/google-modules/touch/sec_touch"]="touch/sec_touch"
+    ["https://android.googlesource.com/kernel/google-modules/touch/synaptics_touch"]="touch/synaptics_touch"
+    ["https://android.googlesource.com/kernel/google-modules/video/gchips"]="video/gchips"
+    ["https://android.googlesource.com/kernel/google-modules/wlan/bcmdhd/bcm4389"]="wlan/bcmdhd/bcm4389"
+    ["https://android.googlesource.com/kernel/google-modules/wlan/syna/dhd43752p"]="wlan/syna/dhd43752p"
+    ["https://android.googlesource.com/kernel/google-modules/wlan/qcom/wcn6740/cnss2"]="wlan/qcom/wcn6740/cnss2"
+    ["https://android.googlesource.com/kernel/google-modules/wlan/qcom/wcn6740/wlan"]="wlan/qcom/wcn6740/wlan"
+    ["https://android.googlesource.com/kernel/google-modules/wlan/wlan_ptracker"]="wlan/wlan_ptracker"
+)
+
+# Regular repositories to clone
+regular_repos=(
+    "https://android.googlesource.com/kernel/google-modules/amplifiers"
+    "https://android.googlesource.com/kernel/google-modules/aoc"
+    "https://android.googlesource.com/kernel/google-modules/aoc_ipc"
+    "https://android.googlesource.com/kernel/google-modules/bms"
+    "https://android.googlesource.com/kernel/google-modules/display"
+    "https://android.googlesource.com/kernel/google-modules/gpu"
+    "https://android.googlesource.com/kernel/google-modules/lwis"
+    "https://android.googlesource.com/kernel/google-modules/nfc"
+    "https://android.googlesource.com/kernel/google-modules/uwb"
+    "https://android.googlesource.com/kernel/devices/google/lynx"
+)
+
+# Clone regular repositories
+for repo in "${regular_repos[@]}"; do
+    echo "Cloning $repo..."
+    git clone --branch android-14.0.0_r0.66 --single-branch "$repo"
+done
+
+# Clone repositories with custom folder structure
+for repo_url in "${!custom_repos[@]}"; do
+    folder_path="${custom_repos[$repo_url]}"
+    clone_and_move "$repo_url" "$folder_path"
+done
